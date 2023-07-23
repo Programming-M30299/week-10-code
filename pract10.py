337e151d72326d6f5c9cab878bae83a5fcdfc202
manighahrmani <mashapicasso@gmail.com>
2023-07-23 02:06:49 +0100

    Update pract10.py

--- pract10.py
+++ pract10.py
@@ -4,17 +4,17 @@ from graphics import *
 def testPoint():
     p = Point(100, 50)
 
-    print("p is of type:", type(p))  # Point
+    print("p is of type:", type(p))  # <class 'graphics.Point'>
 
-    print("p's x coordinate is", p.getX())  # 100
-    print("p's y coordinate is", p.getY())  # 50
+    print("p's x coordinate is", p.getX())  # 100.0
+    print("p's y coordinate is", p.getY())  # 50.0
 
     p.move(10, -20)
 
-    print("p's x coordinate is", p.getX())  # 110
-    print("p's y coordinate is", p.getY())  # 30
+    print("p's x coordinate is", p.getX())  # 110.0
+    print("p's y coordinate is", p.getY())  # 30.0
 
-    print("p is:", p)  # Point(110, 30)
+    print("p is:", p)  # Point(110.0, 30.0)
 
 
 testPoint()
