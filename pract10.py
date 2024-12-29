81f7c338c2241448dcc7cf64b16a42ab78897c78
manighahrmani <mashapicasso@gmail.com>
2024-12-29 23:01:39 +0000

    Update pract10 based on Worksheet changes

--- pract10.py
+++ pract10.py
@@ -8,8 +8,8 @@ class MyPoint:
         self.y = y
 
     def move(self, dx, dy):
-        self.x = self.x + dx
-        self.y = self.y + dy
+        self.x += dx
+        self.y += dy
 
     def __str__(self):
         output = f"MyPoint({self.x}, {self.y})"
@@ -22,7 +22,8 @@ class Square():
         self.p1 = p1
         self.side = side
         self.p2 = MyPoint(p1.x + side, p1.y + side)
-        self.fill_colour = "black"
+        self.fill_colour = "white"
+        self.outline_colour = "black"
 
     def move(self, dx, dy):
         self.p1.move(dx, dy)
@@ -38,28 +39,34 @@ def test_my_point():
     print("my_point's x coordinate is", my_point.x)  # 100
     print("my_point's y coordinate is", my_point.y)  # 50
 
+    my_point.x = 200
+    my_point.y = my_point.y + 100
+    print("my_point's x coordinate is", my_point.x)  # 200
+    print("my_point's y coordinate is", my_point.y)  # 150
+
     my_point.move(10, -20)
+    print("my_point's x coordinate is", my_point.x)  # 210
+    print("my_point's y coordinate is", my_point.y)  # 130
 
-    print("my_point's x coordinate is", my_point.x)  # 110
-    print("my_point's y coordinate is", my_point.y)  # 30
-
-    print("my_point is:", my_point)  # my_point is: MyPoint(110, 30)
+    print("my_point is:", my_point)  # my_point is: MyPoint(210, 130)
 
 
 def test_point():
     p = Point(100, 50)
+    print("p is of type:", type(p))  # <class 'graphix.Point'>
+    print("p's x coordinate is", p.x)  # 100
+    print("p's y coordinate is", p.y)  # 50
 
-    print("p is of type:", type(p))  # p is of type: <class 'graphix.Point'>
-
-    print("p's x coordinate is", p.x)  # 100.0
-    print("p's y coordinate is", p.y)  # 50.0
+    p.x = 200
+    p.y = p.y + 100
+    print("p's x coordinate is", p.x)  # 200
+    print("p's y coordinate is", p.y)  # 150
 
     p.move(10, -20)
+    print("p's x coordinate is", p.x)  # 210
+    print("p's y coordinate is", p.y)  # 130
 
-    print("p's x coordinate is", p.x)  # 110.0
-    print("p's y coordinate is", p.y)  # 30.0
-
-    print("p is:", p)  # p is: Point(110.0, 30.0)
+    print("p is:", p)  # p is: Point(210, 130)
 
 
 def test_square():
@@ -79,6 +86,6 @@ def test_square():
 
     print(square)  # Square(MyPoint(110, 30), MyPoint(160, 80))
 
-    print("Changing square's fill colour default (black) to red ...")
+    print("Changing square's fill colour default (white) to red ...")
     square.fill_colour = "red"
     print("square's fill colour is", square.fill_colour)  # red
