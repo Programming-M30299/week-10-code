532654364d64b4dbeedd41f525b823eeb714b474
manighahrmani <mashapicasso@gmail.com>
2024-12-31 16:53:56 +0000

    Add test function for Square class and update fill colour

--- pract10.py
+++ pract10.py
@@ -34,6 +34,28 @@ class Square():
         return output
 
 
+def test_square():
+    p1 = MyPoint(100, 50)
+    square = Square(p1, 50)
+
+    print("square's side length is", square.side)  # 50
+    print("square's p1 is", square.p1)  # MyPoint(100, 50)
+    print("square's p2 is", square.p2)  # MyPoint(150, 100)
+
+    square.move(10, -20)
+
+    print("After the move ...")
+    print("square's side length is", square.side)  # 50
+    print("square's p1 is", square.p1)  # MyPoint(110, 30)
+    print("square's p2 is", square.p2)  # MyPoint(160, 80)
+
+    print(square)  # Square(MyPoint(110, 30), MyPoint(160, 80))
+
+    print("Changing square's fill colour default (white) to red ...")
+    square.fill_colour = "red"
+    print("square's fill colour is", square.fill_colour)  # red
+
+
 def test_my_point():
     my_point = MyPoint(100, 50)
     print("my_point's x coordinate is", my_point.x)  # 100
@@ -67,25 +89,3 @@ def test_point():
     print("p's y coordinate is", p.y)  # 130
 
     print("p is:", p)  # p is: Point(210, 130)
-
-
-def test_square():
-    p1 = MyPoint(100, 50)
-    square = Square(p1, 50)
-
-    print("square's side length is", square.side)  # 50
-    print("square's p1 is", square.p1)  # MyPoint(100, 50)
-    print("square's p2 is", square.p2)  # MyPoint(150, 100)
-
-    square.move(10, -20)
-
-    print("After the move ...")
-    print("square's side length is", square.side)  # 50
-    print("square's p1 is", square.p1)  # MyPoint(110, 30)
-    print("square's p2 is", square.p2)  # MyPoint(160, 80)
-
-    print(square)  # Square(MyPoint(110, 30), MyPoint(160, 80))
-
-    print("Changing square's fill colour default (white) to red ...")
-    square.fill_colour = "red"
-    print("square's fill colour is", square.fill_colour)  # red
